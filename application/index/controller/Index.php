2e750a85307ce34f335889f58656438b102a3dd6
qingda <1051986458@qq.com>
2021-01-01 21:00:44 +0800

    张庆达

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,6 +1,5 @@
 <?php
 namespace app\index\controller;
-use think\View;
 
 class Index
 {
---
+++ application/index/controller/Indexx.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\indexx\controller;
+
+class Index
+{
+    public function indexx(){
+        return $this->fetch();
+    }
+}
